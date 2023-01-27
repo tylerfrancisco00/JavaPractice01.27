b8537de3619852b00e29a477c03fa07e22369c5c
tylerfrancisco00 <franciscotp@appstate.edu>
2023-01-27 16:53:57 -0500

    Third commit

--- src/Demo.java
+++ src/Demo.java
@@ -1,22 +1,57 @@
+package src;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 
 public abstract class Demo 
 {
-
     abstract void driving();
     abstract void riding();
 
     public static void main(String args[])
     {
-        
+        Demo thingWithWheels;
+        Scanner keyboard = new Scanner(System.in);
+        int wheels = 0;
+        do
+        {
+            try
+            {
+            System.out.println("please enter a number of wheels");
+            wheels = keyboard.nextInt();
+            if (wheels <= 0)
+            {
+                throw new IllegalArgumentException();
+            }
 
 
+            }
+            catch(InputMismatchException e)
+            {
+                System.out.println("Please enter a valid number and try again.");
+                keyboard.nextLine();
+            }
+            catch(IllegalArgumentException e)
+            {
+                System.out.println("Please enter a number greater than zero.");
+                keyboard.nextLine();
+            }
+        }       
+        while (wheels == 0);  
+
+        if (wheels < 4)
+        {
+            thingWithWheels = new Bike();
+            thingWithWheels.riding();
+
+        }
+        else
+        {
+            thingWithWheels = new Car();
+            thingWithWheels.driving();
+        }
+
+        keyboard.close();
     }
-
-
-
-
-
-
 }
